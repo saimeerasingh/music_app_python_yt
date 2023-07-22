215f24a88799d5cca55ea914495902903f2cff28
Saimeera <singhsaimeera@gmail.com>
2023-07-22 18:43:21 +0100

    -video working

--- models/playlist.py
+++ models/playlist.py
@@ -1,20 +1,23 @@
 from models.song import Song
 from ytmusicapi import YTMusic
 
-song1 = Song("Beautiful","One Direction","Romance",True)
-song2 = Song("Shape","Ed","Fitness",True)
-song3 = Song("Haiye","AR","Fast Beat",False)
+# song1 = Song("Beautiful","One Direction","Romance",True)
+# song2 = Song("Shape","Ed","Fitness",True)
+# song3 = Song("Haiye","AR","Fast Beat",False)
 
-playlist = [song1,song2,song3]
+playlist = []
 
 def add_new_song(new_song):
     playlist.append(new_song)
     
 def search_song(new_song):
     ytmusic = YTMusic(auth="oauth.json")
-    results = ytmusic.search(query=new_song)
-    titles = set()
+    results = ytmusic.search(query=new_song,filter="songs",limit=5)
+    song_found = set()
     for item in results:
-      if "title" in item:
-        titles.add(item["title"])
-    return titles
+        filtered_song =Song(
+            name =item.get("title","Unnamed song"),
+            artist_name =item.get("artist","Unknown Artist"),
+            video = item.get("videoId","Video Unavailable"))
+        song_found.add(filtered_song)
+    return song_found
--- models/song.py
+++ models/song.py
@@ -1,8 +1,9 @@
 class Song():
     
-    def __init__(self, name, artist_name,genre,played,thumbnail=None):
+    def __init__(self, name, artist_name,video,played = False,thumbnail=None):
         self.song_name = name
         self.artist_name = artist_name
-        self.song_genre = genre
         self.played = played
+        self.video = video
         self.thumbnail = thumbnail
+       
